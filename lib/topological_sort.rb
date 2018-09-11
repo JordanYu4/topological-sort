97607ee4ea4dc4d5aa1bcb4559dbd4ece25f4d97
Jordan Yu <jordanyu44@gmail.com>
2018-09-11 15:21:21 -0700

    Update README

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -3,5 +3,10 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  return kahns_algorithm(vertices)
+  #return tarjans_algorithm(vertices)
+end
 
+def kahns_algorithm(vertices)
+  queue = []
 end
