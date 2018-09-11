5e1a93de207aff9f14dbe82f1887b476de1daf9a
Jordan Yu <jordanyu44@gmail.com>
2018-09-11 11:36:00 -0700

    Set up install order vetices and edges

--- lib/install_order.rb
+++ lib/install_order.rb
@@ -1,14 +1,20 @@
 # Given an Array of tuples, where tuple[0] represents a package id,
-# and tuple[1] represents its dependency, determine the order in which
-# the packages should be installed. Only packages that have dependencies
-# will be listed, but all packages from 1..max_id exist.
+# and tuple[1] represents its dependency, install_order determines 
+# the order in which the packages should be installed. Only packages 
+# that have dependencies will be listed, but all packages from 
+# 1..max_id exist. 
 
-# N.B. this is how `npm` works.
-
-# Import any files you need to
-
-
+require_relative 'graph'
 
 def install_order(arr)
-
+  vertices = Hash.new(true)
+  edges = []
+  arr.each do |tup|
+    pack_id, dependency = tup[0], tup[1]
+    from_vertex = Vertex.new(dependency) 
+    to_vertex = Vertex.new(pack_id)
+    edges << Edge.new(from_vertex, to_vertex)
+    Hash[to_vertex]
+    Hash[from_vertex] unless Hash[from_vertex]
+  end 
 end
