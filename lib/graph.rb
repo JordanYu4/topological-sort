3eeeb6728249bede4cafe32d4ef1fa02631fa177
Jordan Yu <jordanyu44@gmail.com>
2018-09-11 10:46:56 -0700

    Build Edge class

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,7 +1,7 @@
 class Vertex
   attr_accessor :in_edges, :out_edges
   attr_reader :value
-  
+
   def initialize(value)
     @value = value
     @in_edges = []
@@ -10,6 +10,9 @@ class Vertex
 end
 
 class Edge
+  attr_accessor :from_vertex, :to_vertex
+  attr_reader :cost
+
   def initialize(from_vertex, to_vertex, cost = 1)
     @from_vertex = from_vertex
     @to_vertex = to_vertex
@@ -19,26 +22,15 @@ class Edge
   end
   
   def initialize_edges
-    from_vertex.each do |vertex|
-      vertex.out_edges << self
-    end
-    to_vertex.each do |vertex| 
-      vertex.in_edges << self
-    end
+    from_vertex.out_edges << self
+    to_vertex.in_edges << self
   end
 
   def destroy!
-    from_vertex.each do |vertex|
-      vertex.out_edges.delet(self)
-    end 
-    to_vertex.each do |vertex|
-      vertex.in_edges.delete(self)
-    end
+    from_vertex.out_edges.delete(self)
+    to_vertex.in_edges.delete(self)
     self.from_vertex = nil
     self.to_vertex = nil 
-  end
+  end 
 
-  private 
-
-  attr_accessor :from_vertex, :to_vertex
 end
